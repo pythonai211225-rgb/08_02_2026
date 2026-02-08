55aef4af6fc992e8d6bcf0f198c9ab595d3ade93
fullstack290623 <fullstack290623@gmail.com>
2026-02-08 21:27:41 +0200

    upload

--- 06_exercise.py
+++ 06_exercise.py
@@ -4,26 +4,68 @@ list1 = [1, 3, 5, 1, 7, 9, 2, 2, 8, 5, 10, 5]
 list2 = [1, 2, 5]
 Remove all items from list1 that appear in list2
 Print list1 at the end
+'''
+list1 = [1, 3, 5, 1, 7, 9, 2, 2, 8, 5, 10, 5]
+list2 = [1, 2, 5]
+for item in list2:
+    while item in list1:
+        list1.remove(item)
 
+print(list1)
+
+
+'''
 2
 list1 = [1, 2, 9, 88, 0]
 list2 = [3, 4, -3]
 list3 = [5, 6, 55]
 Create list4 that contains all values from list1, list2, and list3
 Use loops only (no +)
+'''
+list1 = [1, 2, 9, 88, 0]
+list2 = [3, 4, -3]
+list3 = [5, 6, 55]
+list4 = []
+for item in list1:
+    list4.append(item)
+for item in list2:
+    list4.append(item)
+for item in list3:
+    list4.append(item)
 
+print(list4)
+
+'''
 3
 list1 = [1, 2, 3, 4, 5]
 list2 = [2, 4]
 Create a new list that contains items from list1
 that do NOT appear in list2
+'''
+list1 = [1, 2, 3, 4, 5]
+list2 = [2, 4]
+list3 = []
+for item in list1:
+    if not item in list2:
+        list3.append(item)
+print(list3)
 
+'''
 4
 list1 = [1, 2, 3, 4, 5]
 list2 = [3, 4, 6, 7]
 Create a list with items that appear in both lists
 No duplicates in the result
+'''
+list1 = [1, 2, 3, 4, 5]
+list2 = [3, 4, 6, 7]
+list3 = []
+for item in list1:
+    if item in list2:
+        list3.append(item)
+print(list3)
 
+'''
 5
 list1 = [1, 2, 3, 4, 5, 6, 7]
 list2 = [2, 4]
@@ -31,7 +73,7 @@ list3 = [5, 6]
 Remove from list1 all items that appear in either list2 or list3
 Print list1
 
-6
+6 ****BONUS
 list1 = [1, 4, 6] -- sorted
 list2 = [2, 5, 7] -- sorted
 Create list3 that contains all numbers from both lists but keep them sorted
