8defcd39d68761d582a65bcc320f45e6fff714e9
fullstack290623 <fullstack290623@gmail.com>
2026-02-08 18:16:46 +0200

    upload

---
+++ 03_lists.py
@@ -0,0 +1,31 @@
+
+grade: int = 100
+temperature: float = 37.5
+is_adult: bool = True
+name: str = 'Danny!'
+
+# input all grades for class with 30 students
+# print all grades
+grades: list[int] = [100, 92, 99, 80]
+names: list[str] = ['danny', 'moshe', 'suzi', 'sharon']
+working: list[bool] = [True, False, True, True]
+floats: list[float] = [3.4, 9.9999, -7.1]
+comb: list[any] = [3.7, 1, 'danny', True]
+
+list_of_lists: list[ list[int] ] = [ [], [1,2,3], [7,7,7,7,7,7] ]
+
+list_numbers: list[int] = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+# use the ':' --> each print 1 line of code
+# print the numbers from 3-9
+# print from start-5
+# print from 6-end
+# print all even numbers (zuggim)
+# print all odd numbers (ei-zugim)
+# print all multiple 5 numbers (mahpela 5)
+# print in reverse
+
+# print
+
+print(grades)
+print(working)
+print(list_of_lists)
